7af6934a97778c0d86facd1a263e307fd29c01cc
pythonx4 <colm.gallagher5@gmail.com>
2013-05-20 21:32:27 -0300

    Update Final.py

--- Final.py
+++ Final.py
@@ -92,3 +92,28 @@ def graph(function, xmin=-10, xmax=10, accuracy=100):
         x+=i
     pylab.plot(points[0],points[1], '-')
     pylab.show()
+def tent():
+    def cost(length, base, height):
+        return 1.4*base*height + 3.8*base*length
+    def volume(length,base,height):
+        return .5*base*height*length
+    def get_specs():
+        the_cost = 9001
+        specification = []
+        base = 0.0001
+        while base < 4.4:
+            height = (math.sqrt(3)/2)*base
+            length = (8.8/math.sqrt(3))/(base**2)
+            temp_cost = cost(length,base,height)
+            if temp_cost < the_cost:
+                the_cost = temp_cost
+                specification = [round(length,2),round(base,2),round(height,2)]
+            base+=.0001
+        return specification
+
+    specs = get_specs()
+    print 'Base: '+str(specs[1])
+    print 'Length: '+str(specs[0])
+    print 'Height: '+str(specs[2])
+    print 'Cost: '+str(round(cost(specs[0],specs[1],specs[2]),2))
+    print 'Volume: '+str(round(volume(specs[0],specs[1],specs[2]),2))
